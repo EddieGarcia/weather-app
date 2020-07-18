c7c39b23549f48c6615c47be0328b7c74131c6a2
Eduard Kubinec <eduard.kubinec@sap.com>
2020-07-18 11:10:02 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import { AddCardComponent } from './components/add-card/add-card.component';
+import { DetailsComponent } from './components/details/details.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherCardComponent,
+    AddCardComponent,
+    DetailsComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
